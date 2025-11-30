28fbfa417fb790136056a46008061ab414256810
mukhammaddiyor <iiim97838@gmail.com>
2025-11-30 21:15:13 +0500

    platzi

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ export default function App() {
   const [users, setUsers] = useState([]);
 
   const getProducts = async () => {
-    const res = await api.get("https://api.escuelajs.co/api/v1/products");
+    const res = await api.get("products");
     setProducts(res.data);
   };
 
   const getUsers = async () => {
-    const res = await api.get("https://api.escuelajs.co/api/v1/users");
+    const res = await api.get("users");
     setUsers(res.data);
   };
 
